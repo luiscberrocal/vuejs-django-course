f56b3917d8a76718d002920744a1818a2a44a334
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-01-28 17:28:28 -0500

    Chapter 20

--- vuejs-frontend/src/app.js
+++ vuejs-frontend/src/app.js
@@ -1,8 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: "Finish course"
+            courseGoal: "Finish course",
+            vueLink: "https://google.com/"
         };
+    },
+    methods: {
+        outputGoal(){
+            const  n = Math.random()
+            if (n > 0.5){
+                return 'Learn Vue'
+            } else {
+                return 'Master Vueu'
+            }
+        }
     }
 });
 
