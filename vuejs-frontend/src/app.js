490e9fcfe3aa813cfe2a392c05e72879bcd37ff4
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-01-29 18:24:26 -0500

    Finshed

--- vuejs-frontend/src/app.js
+++ vuejs-frontend/src/app.js
@@ -3,7 +3,8 @@ const app = Vue.createApp(
         data() {
             return {
                 name: "Luis C. Berrocal",
-                age: 57
+                age: 57,
+                imageLink: "https://cdn.pixabay.com/photo/2016/02/10/16/37/cat-1192026_640.jpg"
             }
         },
         methods: {
