e93941229bbc1bca982dac036537b9af9bb595fb
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-01-29 18:20:09 -0500

    First part

--- vuejs-frontend/src/app.js
+++ vuejs-frontend/src/app.js
@@ -1,21 +1,20 @@
-const app = Vue.createApp({
-    data() {
-        return {
-            courseGoal: "Finish course",
-            vueLink: "https://google.com/"
-        };
-    },
-    methods: {
-        outputGoal(){
-            const  n = Math.random()
-            if (n > 0.5){
-                return 'Learn Vue'
-            } else {
-                return 'Master Vueu'
+const app = Vue.createApp(
+    {
+        data() {
+            return {
+                name: "Luis C. Berrocal",
+                age: 57
+            }
+        },
+        methods: {
+            ageIn5Years() {
+                return this.age + 5;
+            },
+            getRandom() {
+                return Math.random();
             }
         }
     }
-});
+)
 
-app.mount('#user-goal');
-
+app.mount("#assignment")
