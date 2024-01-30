b981493f502e576ab0518acd393709eb64999cd5
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-01-30 18:43:53 -0500

    Unsolved

--- vuejs-frontend/src/app.js
+++ vuejs-frontend/src/app.js
@@ -1,12 +1,26 @@
 const app = Vue.createApp(
     {
         data() {
-            return {};
+            return {
+                styleNum: 0
+            };
         },
         watch: {},
-        computed: {},
-        methods: {}
+        computed: {
+            currentStyle() {
+                console.log("Setting current style",  this.styleNum);
+                if (this.styleNum === 0) {
+                    return "";
+                } else if (this.styleNum === 1 || this.styleNum === 2) {
+                    return "user" + this.styleNum;
+                } else {
+                    return "";
+                }
+            }
+        },
+        methods: {
+        }
     }
 )
 
-app.mount("#assignment"
+app.mount("#assignment")
