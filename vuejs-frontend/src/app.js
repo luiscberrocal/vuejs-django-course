588ed33b983b25eb10f7efd51fb0699c9c1893f2
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-01-30 18:28:53 -0500

    3rd asssignment

--- vuejs-frontend/src/app.js
+++ vuejs-frontend/src/app.js
@@ -1,36 +1,12 @@
 const app = Vue.createApp(
     {
         data() {
-            return {
-                counter: 0,
-            };
+            return {};
         },
-        watch: {
-            message() {
-                const that = this;
-                setTimeout(function(){
-                    that.counter = 0;
-                }, 5000);
-            }
-        },
-        computed: {
-            message(){
-                 if (this.counter < 37) {
-                    return "Not there yet";
-                }
-                if (this.counter > 37){
-                    return "Too much";
-                }
-
-            }
-        },
-        methods: {
-            add(value) {
-                this.counter = this.counter + value;
-            },
-        }
+        watch: {},
+        computed: {},
+        methods: {}
     }
 )
 
-app.mount("#assignment")
-
+app.mount("#assignment"
