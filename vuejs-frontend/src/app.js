261459cb04e952d6102d2e02d958bfb3b2e47eca
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-02 17:32:27 -0500

    Attacks

---
+++ vuejs-frontend/src/app.js
@@ -0,0 +1,29 @@
+function getAttackValue(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+const app = Vue.createApp(
+    {
+        data() {
+            return {
+                playerHealth: 100,
+                monsterHealth: 100
+            };
+        },
+        watch: {},
+        computed: {},
+        methods: {
+            attackMonster() {
+                const attackValue = getAttackValue(8, 12);
+                this.monsterHealth -= attackValue;
+                this.attackPlayer();
+            },
+            attackPlayer() {
+                const attackValue = getAttackValue(10, 18);
+                this.playerHealth -= attackValue;
+            },
+        }
+    }
+)
+
+app.mount("#game")
