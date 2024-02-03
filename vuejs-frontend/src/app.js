657d123d4bbfbeaa7ab3fdd027f1943b14b4e204
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-03 08:16:25 -0500

    New game buttons.

--- vuejs-frontend/src/app.js
+++ vuejs-frontend/src/app.js
@@ -72,6 +72,12 @@ const app = Vue.createApp(
                     this.playerHealth += healValue;
                 }
                 this.attackPlayer()
+            },
+            newGame() {
+                this.playerHealth = 100;
+                this.monsterHealth = 100;
+                this.currentRound = 0;
+                this.winner = null;
             }
         }
     }
