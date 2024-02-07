f8f327433b6a3582e950383888b3069fb24f72c0
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-07 09:00:28 -0500

    User data form

--- vuejs-frontend/src/src/components/UserData.vue
+++ vuejs-frontend/src/src/components/UserData.vue
@@ -1,11 +1,23 @@
-<script >
-
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      age: null
+    }
+  }
+}
 </script>
 
 <template>
 
   <div>
     <h2>User Data</h2>
+    <form @submit.prevent>
+      <input type="text" v-model="name">
+      <input type="number" v-model="age">
+      <button>Save</button>
+    </form>
   </div>
 </template>
 
