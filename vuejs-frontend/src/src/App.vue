dec7032c0bc341e10c915cdb67df08219e67de68
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-08 06:03:21 -0500

    Created local components

--- vuejs-frontend/src/src/App.vue
+++ vuejs-frontend/src/src/App.vue
@@ -3,15 +3,24 @@
     <the-header></the-header>
     <badge-list></badge-list>
     <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
+        :full-name="activeUser.name"
+        :info-text="activeUser.description"
+        :role="activeUser.role"
     ></user-info>
   </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import UserInfo from './components/UserInfo.vue';
+import BadgeList from './components/BadgeList.vue';
+
 export default {
+  components: {
+    TheHeader,
+    UserInfo,
+    BadgeList
+  },
   data() {
     return {
       activeUser: {
