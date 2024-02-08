07a61a441a57069d0d1c75304b45731e490445d5
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-08 05:05:54 -0500

    Finished user project

---
+++ vuejs-frontend/lessons/cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,18 @@
+<script >
+export default {
+  props: [
+    'name',
+    'age'
+  ]
+}
+</script>
+<template>
+  <section>
+    <h2>{{ name }}</h2>
+      <h3>{{ age }}</h3>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
