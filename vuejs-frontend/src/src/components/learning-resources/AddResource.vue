5026e835bc73c5c16b5096ddedf9b67e060731eb
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-11 10:06:06 -0500

    Added AddResource and TheResource components

---
+++ vuejs-frontend/src/src/components/learning-resources/AddResource.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    props: [],
+    data() {
+        return {};
+    },
+    methods: {},
+    computed: {}
+}
+</script>
+
+<template>
+<h1>Add Resource</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vuejs-frontend/src/src/components/learning-resources/TheResource.vue
@@ -0,0 +1,32 @@
+<script>
+import StoredResources from "./StoredResources.vue";
+import AddResource from "./AddResource.vue";
+
+export default {
+  components: {StoredResources, AddResource},
+  props: [],
+  data() {
+    return {
+      selectedTab: 'stored-resources'
+    };
+  },
+  methods: {
+    setSelectTab(tab) {
+      this.selectedTab = tab;
+    }
+  },
+  computed: {}
+}
+</script>
+
+<template>
+  <base-card>
+    <base-button @click="setSelectTab('stored-resources')">Stored Resources</base-button>
+    <base-button @click="setSelectTab('add-resource')">Add Resource</base-button>
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<style scoped>
+
+</style>
