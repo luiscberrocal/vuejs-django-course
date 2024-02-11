f3b14d25c8fe71195653cbc6b48c965be7166648
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-11 08:08:00 -0500

    Added App vue

---
+++ vuejs-frontend/src/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Vue.js  Official Guide',
+          date: '2021-07-17',
+          description: 'The Official Guide to Vue.js',
+          link: 'https://vuejs.org',
+        },
+      ],
+    };
+  },
+  methods: {}
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
