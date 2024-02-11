020517a3f2e9b049484ef8a8a6568afa8d54e37e
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-11 12:43:05 -0500

    Finished Resource project

---
+++ vuejs-frontend/lessons/prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
@@ -0,0 +1,90 @@
+<script>
+export default {
+  props: [],
+  data() {
+    return {
+      inputIsInvalid: false
+    };
+  },
+  inject: ['addResource'],
+  methods: {
+    submitData() {
+      console.log('Append Resource.....');
+      const title = this.$refs.titleInput.value;
+      const description = this.$refs.descriptionInput.value;
+      const link = this.$refs.linkInput.value;
+      this.inputIsInvalid = title.trim() === '' || description.trim() === '' || link.trim() === ''
+      if (this.inputIsInvalid) {
+        return;
+      }
+      this.addResource(title, description, link);
+
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    }
+  }
+}
+</script>
+
+<template>
+  <base-dialog v-if="inputIsInvalid" title="Invalid Data" @close="confirmError">
+    <template #default>
+      <p>Please enter all data required.</p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Ok</base-button>
+    </template>
+  </base-dialog>
+  <base-card>
+    <form @submit.prevent="submitData">
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input type="text" id="title" ref="titleInput">
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea id="description" rows="3" ref="descriptionInput"></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input type="url" id="link" ref="linkInput">
+      </div>
+      <div class="form-control">
+        <base-button
+            type="submit"
+        >Add Resource
+        </base-button>
+      </div>
+    </form>
+  </base-card>
+</template>
+
+<style scoped>
+
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
---
+++ vuejs-frontend/lessons/prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,69 @@
+<script>
+
+
+export default {
+  inject: ['removeResource'],
+  props: [
+    'id',
+    'title',
+    'description',
+    'link'
+  ],
+  data() {
+    return {};
+  },
+  methods: {}
+}
+</script>
+
+<template>
+  <li>
+    <base-card>
+      <header>
+        <h3>{{ title }}</h3>
+        <div>
+          <base-button @click="removeResource(id)">Delete</base-button>
+          <base-button>Check</base-button>
+          <base-button mode="flat">Share</base-button>
+        </div>
+      </header>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link" target="_blank">View resource</a>
+      </nav>
+    </base-card>
+  </li>
+</template>
+
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
+
+</style>
