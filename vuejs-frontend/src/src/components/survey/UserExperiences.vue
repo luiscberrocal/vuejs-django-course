9f1214de72479ca0ef165e430c1ec1bbb6dbae3b
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-12 08:18:38 -0500

    Fecthing data from Firebase.

--- vuejs-frontend/src/src/components/survey/UserExperiences.vue
+++ vuejs-frontend/src/src/components/survey/UserExperiences.vue
@@ -3,14 +3,14 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <ul>
         <survey-result
-          v-for="result in results"
-          :key="result.id"
-          :name="result.name"
-          :rating="result.rating"
+            v-for="result in results"
+            :key="result.id"
+            :name="result.name"
+            :rating="result.rating"
         ></survey-result>
       </ul>
     </base-card>
@@ -21,9 +21,40 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  data() {
+    return {
+      results: [],
+    };
+  },
   components: {
     SurveyResult,
+  },
+  methods: {
+    loadExperiences() {
+      const url = "https://vuejs-course-640bf-default-rtdb.firebaseio.com/surveys.json";
+      console.log('Calling ', url)
+      fetch(url).then(
+          (response) => {
+            console.log('Processing response', response)
+            if (response.ok) {
+              return response.json();
+            }
+          }
+      ).then((data) => {
+        console.log('Processing data', data)
+        const results = [];
+        for (let key in data) {
+          const result = data[key];
+          results.push({
+            id: key,
+            name: result.userName,
+            rating: result.rating,
+          });
+          this.results = results;
+        }
+        console.log(results);
+      })
+    },
   },
 };
 </script>
