4bfc80d96df90b431cb4295025e51dbb8ff4b293
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-13 07:25:00 -0500

    Nested routes

--- vuejs-frontend/src/src/components/teams/TeamsList.vue
+++ vuejs-frontend/src/src/components/teams/TeamsList.vue
@@ -1,11 +1,12 @@
 <template>
+  <router-view></router-view>
   <ul>
     <teams-item
-      v-for="team in teams"
-      :key="team.id"
-      :id="team.id"
-      :name="team.name"
-      :member-count="team.members.length"
+        v-for="team in teams"
+        :key="team.id"
+        :id="team.id"
+        :name="team.name"
+        :member-count="team.members.length"
     ></teams-item>
   </ul>
 </template>
