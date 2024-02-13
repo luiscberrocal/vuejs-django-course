ed8c9703a228351c0b0920a67511637fd7c69626
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-13 12:19:48 -0500

    New frontedn

---
+++ frontend/src/components/users/UserFooter.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: [],
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {}
+}
+</script>
+
+<template>
+  <h4>User Footer</h4>
+</template>
+
+<style scoped>
+
+</style>
