469ffd6360659df5fc9f259e37550b90b72d2327
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-14 07:57:49 -0500

    Recurrent payment item component.

---
+++ vuejs-frontend/src/src/components/payments/RecurrentPaymentItem.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  props: ['recurrentPayment'],
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {}
+}
+</script>
+
+<template>
+  <li>
+    <h3>{{ recurrentPayment.name }}</h3>
+  </li>
+</template>
+
+<style scoped>
+
+</style>
