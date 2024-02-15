735ab87213a315b1b91ad5ffc3a55bb67bf302e6
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-14 19:06:46 -0500

    Moved app to src

--- vuejs-frontend/src/src/main.js
+++ vuejs-frontend/src/src/main.js
@@ -1,32 +1,7 @@
-import {createApp} from 'vue';
-import {createRouter, createWebHistory} from 'vue-router';
+import { createApp } from 'vue';
+
 import App from './App.vue';
-import UsersList from "./components/users/UsersList.vue";
-import TeamMembers from "./components/teams/TeamMembers.vue";
-import TeamFooter from "./components/teams/TeamFooter.vue";
-import UserFooter from "./components/users/UserFooter.vue";
-import TeamsList from "./components/teams/TeamsList.vue";
-import PaymentList from "./components/payments/PaymentList.vue";
 
-const app = createApp(App)
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {path: '/', redirect: '/teams'},
-        {
-            path: '/teams',
-            components: {default: TeamsList, footer: TeamFooter},
-            children: [
-                {name: 'team-members', path: ':teamId', component: TeamMembers, props: true},
-            ]
-        },
-        {path: '/users', components: {default: UsersList, footer: UserFooter}},
-        {path: '/payments', components: {default: PaymentList}},
-        {path: '/:notFound(.*)', redirect: '/teams'}
-    ],
-    linkActiveClass: 'active'
-})
-
-app.use(router);
+const app = createApp(App);
 
 app.mount('#app');
