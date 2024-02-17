c99cb32f4b2d48d1503973796fe7f3408123bdcf
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-17 06:35:33 -0500

    NuxtLinks implementation

--- my-nuxt-app/pages/index.vue
+++ my-nuxt-app/pages/index.vue
@@ -1,16 +1,33 @@
 <script lang="ts">
 export default {
-    props: [],
-    data() {
-        return {};
-    },
-    methods: {},
-    computed: {}
+  props: [],
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {}
 }
 </script>
 
 <template>
-<h1>Home</h1>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/products">Products</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+  <h2>Home</h2>
   <p>My application</p>
 </template>
 
