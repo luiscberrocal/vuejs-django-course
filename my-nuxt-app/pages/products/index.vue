b0818c0d17b34ae1aadc141c6bf91729423c8345
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-17 08:07:42 -0500

    Connecting to FakeStore

--- my-nuxt-app/pages/products/index.vue
+++ my-nuxt-app/pages/products/index.vue
@@ -2,12 +2,18 @@
 definePageMeta({
   layout: "products",
 });
+const {data: products} = await useFetch('https://fakestoreapi.com/products')
 </script>
 
 <template>
   <h1>Products</h1>
-  <p>My products</p>
+  <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+          <NuxtLink :to="`products/${p.id}`">{{p.title}}</NuxtLink>
+      </div>
+  </div>
 </template>
+
 
 <style scoped>
 .router-link-exact-active {
