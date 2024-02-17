8f0c84ee49e4a3de0c8a556284cce9a3c1f0d672
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-17 11:44:23 -0500

    Error management

--- my-nuxt-app/pages/products/[id].vue
+++ my-nuxt-app/pages/products/[id].vue
@@ -2,7 +2,9 @@
 const {id} = useRoute().params;
 const uri = `https://fakestoreapi.com/products/${id}`;
 const {data: product} = await useFetch(uri, {key: id});
-
+if (!product.value) {
+  throw createError({statusCode: 404, statusMessage: 'Product not found'});
+}
 definePageMeta({
   layout: "products",
 });
