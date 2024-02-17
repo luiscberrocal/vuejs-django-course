7c4206be369604776de0dd2ba55ad9e00f2d964d
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-17 06:16:17 -0500

    Pages

--- my-nuxt-app/app.vue
+++ my-nuxt-app/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+      <h1>Main</h1>
+      <p>Hello Ninjas!!</p>
   </div>
 </template>
---
+++ my-nuxt-app/pages/about.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+export default {
+    props: [],
+    data() {
+        return {};
+    },
+    methods: {},
+    computed: {}
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
