ab76bd3cd33c1f6c57a2e06a64fc0b871bd52458
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-17 08:37:59 -0500

    Changes to product cards

--- my-nuxt-app/pages/products/index.vue
+++ my-nuxt-app/pages/products/index.vue
@@ -8,9 +8,10 @@ const {data: products} = await useFetch('https://fakestoreapi.com/products')
 <template>
   <h1>Products</h1>
   <div class="grid grid-cols-4 gap-5">
-      <div v-for="p in products">
-          <NuxtLink :to="`products/${p.id}`">{{p.title}}</NuxtLink>
-      </div>
+    <div v-for="p in products">
+      <ProductCard :product="p" :key="p.id"/>
+
+    </div>
   </div>
 </template>
 
