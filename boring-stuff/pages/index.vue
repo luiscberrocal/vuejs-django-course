099894bd6aa10506dadb36c50f8b9efe80f6da88
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-19 14:24:24 -0500

    Index page and tailwind installed.

---
+++ boring-stuff/pages/index.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+export default {
+  props: [],
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {}
+}
+</script>
+
+<template>
+  <div>
+    <h1>Boring stuff</h1>
+    <p>Some boring stuff</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
