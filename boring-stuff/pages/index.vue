c054f057ab2deb577b0cb0d97ce7242e2ec9da10
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-19 17:22:17 -0500

    Cannot read Django endpoint.

--- boring-stuff/pages/index.vue
+++ boring-stuff/pages/index.vue
@@ -1,12 +1,9 @@
-<script lang="ts">
-export default {
-  props: [],
-  data() {
-    return {};
-  },
-  methods: {},
-  computed: {}
-}
+<script lang="ts" setup>
+const url = 'http://127.0.0.1:8000/api/payments/recurrent-payments/'
+console.log(url)
+// const {data: recurrentPayments} = await useFetch(url).catch(err => console.log(err));
+const recurrentPayments = await useFetch(url).catch(err => console.log(err));
+console.log(recurrentPayments)
 </script>
 
 <template>
@@ -18,6 +15,14 @@ export default {
         <header class="bg-gray-500 text-white p-4 rounded-lg">
           Recurrent Payments
         </header>
+        <p>Payment: {{ recurrentPayments }}</p>
+        <ul>
+
+          <li v-for="payment in recurrentPayments" :key="payment.id">
+            <p>{{ payment.name }}</p>
+            <p>{{ payment.amount }}</p>
+          </li>
+        </ul>
       </div>
       <div>
         <header class="bg-gray-500 text-white">
