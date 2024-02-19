e3891c62e803f517bf6cd34f906ebca8eccf7557
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-19 16:45:15 -0500

    Layouts

---
+++ boring-stuff/pages/about.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  props: [],
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {}
+}
+</script>
+
+<template>
+  <h2>About</h2>
+  <p>About the Boring Stuff</p>
+</template>
+
+<style scoped>
+
+</style>
