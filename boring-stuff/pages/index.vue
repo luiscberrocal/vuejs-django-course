2c68672717e379f109df28040baba56742152149
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-19 16:34:25 -0500

    Index vue

--- boring-stuff/pages/index.vue
+++ boring-stuff/pages/index.vue
@@ -14,8 +14,16 @@ export default {
     <h1>Boring stuff</h1>
     <p>Some boring stuff</p>
     <div class="grid grid-cols-2 gap-2">
-      <div>Recurrent Payments</div>
-      <div>Payments</div>
+      <div>
+        <header class="bg-gray-500 text-white p-4 rounded-lg">
+          Recurrent Payments
+        </header>
+      </div>
+      <div>
+        <header class="bg-gray-500 text-white">
+          Payments
+        </header>
+      </div>
 
     </div>
   </div>
