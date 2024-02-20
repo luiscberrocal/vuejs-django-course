a608a95bb82016ef748178f2cc7f5604ea5c5801
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-20 07:06:55 -0500

    List payments

--- boring-stuff/components/Payments/List.vue
+++ boring-stuff/components/Payments/List.vue
@@ -5,13 +5,20 @@ const {recurrentPayments} = defineProps(['recurrentPayments']);
 <template>
   <ul>
     <li v-for="payment in recurrentPayments" :key="payment.id">
-      <div class="flex card justify-between">
-        <h3 class="text-xl font-bold">{{ payment.name }}</h3>
+      <div class="card m-3">
+        <div class="flex justify-between">
+          <h3 class="text-xl font-bold">{{ payment.name }}</h3>
+          <div>
+            <button class="btn mx-0.5">Pay</button>
+            <button class="btn mx-0.5">Edit</button>
+          </div>
+        </div>
         <div>
-          <button class="btn">Edit</button>
+          <p>Amount: ${{ payment.amount }} <span v-if="payment.is_amount_fixed">(Fixed)</span></p>
+          <p>Date due: {{ payment.date_due }}</p>
+          <p>Is Active: {{ payment.is_active }}</p>
         </div>
       </div>
-      <p>{{ payment.amount }}</p>
     </li>
   </ul>
 </template>
