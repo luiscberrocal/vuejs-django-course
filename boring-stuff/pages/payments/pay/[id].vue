bd1a9c39874e3d4692bbd46ddd5f7c0ba1b02a2a
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-20 10:40:20 -0500

    Model save form payment.

--- boring-stuff/pages/payments/pay/[id].vue
+++ boring-stuff/pages/payments/pay/[id].vue
@@ -2,20 +2,42 @@
 const now = new Date().toISOString();
 
 const {id} = useRoute().params;
+
+const url = `http://127.0.0.1:8000/api/payments/recurrent-payments/${id}/`
+const {data: recurrentPayment, error, status} = await useFetch(url);
+//let amount = await recurrentPayment.amount;
+// console.log('NEW AMOUNT', amount)
+const postPayment = async () => {
+  console.log(recurrentPayment)
+
+  //  const url = `http://`
+  //  const response = await fetch(url, {
+  //    method: 'POST',
+  //    headers: {
+  //      'Content-Type': 'application/json'
+  //    },
+  //    body: JSON.stringify({
+  //      date: now,
+  //      amount: recurrentPayment.amount
+  //    })
+  //  })
+};
+
 </script>
 
 <template>
-  <h2>Make Payment {{ id}}</h2>
+  <h2>Make Payment for {{ recurrentPayment.name }}</h2>
   <div>
-    <form action="">
+    <form @submit.prevent="postPayment">
       <div class="form-group">
         <label for="date">Date:</label>
-        <input type="text" id="date" class="form-control" :value="now">
+        <input type="text" id="date" class="form-control" v-model="now">
       </div>
       <div class="form-group mt-3">
         <label for="amount">Amount:</label>
-        <input type="number" id="amount" class="form-control">
+        <input type="text" id="amount" class="form-control" v-model="recurrentPayment.amount">
       </div>
+      <button type="submit">Save</button>
     </form>
   </div>
 </template>
