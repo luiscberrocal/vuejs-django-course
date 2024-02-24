c50375b4be38330cd85cf568d2bf83c92d5340cb
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-24 08:43:28 -0500

    Minor model changes.

--- vuejs_django_course/payments/admin.py
+++ vuejs_django_course/payments/admin.py
@@ -3,17 +3,28 @@ from django.contrib import admin
 from .models import RecurrentPayment, Payment
 
 
+class AuditableAdminMixin:
+    readonly_fields = ('created_by', 'modified_by')
+
+    def save_model(self, request, obj, form, change):
+        if obj.pk:
+            obj.modified_by = request.user
+        else:
+            obj.created_by = request.user
+        super(AuditableAdminMixin, self).save_model(request, obj, form, change)
+
+
 # Register your models here.
 
 @admin.register(RecurrentPayment)
-class RecurrentPaymentAdmin(admin.ModelAdmin):
+class RecurrentPaymentAdmin(AuditableAdminMixin, admin.ModelAdmin):
     list_display = ('name', 'amount', 'date_due', 'is_active', 'is_amount_fixed')
     search_fields = ('name', 'description')
     list_filter = ('is_active', 'is_amount_fixed', 'date_due')
 
 
 @admin.register(Payment)
-class PaymentAdmin(admin.ModelAdmin):
+class PaymentAdmin(AuditableAdminMixin, admin.ModelAdmin):
     list_display = ('recurrent_payment', 'date', 'amount')
     search_fields = ('recurrent_payment__name', 'recurrent_payment__description')
     list_filter = ('date', 'recurrent_payment__is_active', 'recurrent_payment__is_amount_fixed')
--- vuejs_django_course/payments/models.py
+++ vuejs_django_course/payments/models.py
@@ -33,6 +33,10 @@ class Payment(AuditableModel, TimeStampedModel):
     recurrent_payment = models.ForeignKey(RecurrentPayment, on_delete=models.CASCADE)
     date = models.DateTimeField(help_text='Date of the payment')
     amount = models.DecimalField(max_digits=10, decimal_places=2, help_text='Amount of the payment')
-
+    comments = models.TextField(null=True, blank=True, help_text='Comments of the payment')
+    
     def __str__(self):
         return f'{self.recurrent_payment.name} - {self.date}'
+
+    class Meta:
+        ordering = ('date',)
