42ab251c1662bfeb97da41ddf8553b6412da562f
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-27 18:21:22 -0500

    Color button

--- portfolio/components/color-mode-selector.vue
+++ portfolio/components/color-mode-selector.vue
@@ -1,8 +1,24 @@
 <script lang="ts" setup>
+
+const colorMode = useColorMode();
+const modes = [
+  'system',
+  'dark',
+  'light'
+]
+const nextMode = computed(() => {
+  const i = modes.indexOf(colorMode.preference);
+  const nextModeValue = modes[(i + 1) % modes.length];
+  return nextModeValue;
+});
+
+const toggleColorMode = () => {
+  colorMode.preference = nextMode.value;
+}
 </script>
 
 <template>
-<div>Color Change</div>
+  <button @click="toggleColorMode">{{ nextMode }}</button>
 </template>
 
 <style scoped>
