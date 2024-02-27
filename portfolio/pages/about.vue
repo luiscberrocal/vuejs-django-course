5d2a520f4ad8b5b3fc98c305d2ad5b9b739e6a79
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-27 12:51:16 -0500

    NuxtLinks

---
+++ portfolio/pages/about.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "about"
+})
+</script>
+
+<template>
+  <div>
+    <h1>About</h1>
+    <p>This is the about page</p>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ portfolio/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+  <div>
+    <h1>Test</h1>
+    <p>Bla bla</p>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
