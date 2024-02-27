a47ba1b7687a3a42ce9d81583b8415593e561b2d
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-27 14:21:15 -0500

    Default layyout

--- portfolio/app.vue
+++ portfolio/app.vue
@@ -1,15 +1,9 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <NuxtLink to="/">Home</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/about">About</NuxtLink>
-      </li>
-    </ul>
-  </nav>
   <div>
-    <NuxtPage/>
+      <NuxtLayout>
+          <NuxtPage/>
+      </NuxtLayout>
   </div>
 </template>
+<script setup lang="ts">
+</script>
---
+++ portfolio/layouts/default.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+      </ul>
+      <slot></slot>
+    </nav>
+
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
