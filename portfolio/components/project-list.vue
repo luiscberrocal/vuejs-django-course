5639f8254e2ab01e8cdcaa6ef17bd440d7b8f2b2
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-27 17:42:15 -0500

    Project list

--- portfolio/components/project-list.vue
+++ portfolio/components/project-list.vue
@@ -1,15 +1,36 @@
 <script lang="ts" setup>
 
-let projectUrl = 'https://api.github.com/users/luiscberrocal/repos';
-const {error, pending, data} = await useFetch(projectUrl);
+let projectUrl = 'https://api.github.com/users/luiscberrocal/repos?per_page=100';
+const {error, pending, data} = await useFetch(projectUrl, {
+  onResponse(context) {
+    console.log('>>>>>>>')
+    console.log(context.response.headers);
+  },
 
+});
+const repos = computed(() => data.value.filter(repo => repo.description)
+    .sort((a, b) => b.stargazers_count - a.stargazers_count));
 </script>
 
 <template>
   <div>
     <section v-if="pending">Loading...</section>
     <section v-else-if="error">Error: {{ error.message }}</section>
-    <section v-else>{{ data}}</section>
+    <section v-else>
+      <ul class="grid grid-cols-1 gap-4">
+        <li v-for="repo in repos" class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 mono-font"
+            :key="repo.id">
+          <a :href="repo.html_url" target="_blank">
+            <div class="flex justify-between items-center">
+              <h2 class="text-xl font-bold">{{ repo.name }}</h2>
+              <div>{{ repo.stargazers_count }} *</div>
+            </div>
+            <p>{{ repo.description }}</p>
+          </a>
+        </li>
+      </ul>
+
+    </section>
 
   </div>
 
