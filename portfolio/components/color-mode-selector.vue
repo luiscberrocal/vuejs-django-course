99bc4d14a690a1c1dffb030d82ebd02e5edf77a8
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-27 18:45:24 -0500

    Colo selector label.

--- portfolio/components/color-mode-selector.vue
+++ portfolio/components/color-mode-selector.vue
@@ -20,12 +20,18 @@ const nextModeIcon = computed(() => nextModeIcons[nextMode.value])
 const toggleColorMode = () => {
   colorMode.preference = nextMode.value;
 }
+const displayNextMode = ref(false);
 </script>
 
 <template>
-  <button @click="toggleColorMode"
-          class="hover:bg-gray-200 dark:hover:bg-gray-600 px-2 py-1 text-gray-500">{{ nextModeIcon }}
-  </button>
+  <div class="flex items-center space-x-2">
+    <div class="text-gray-500 text-xs" v-if="displayNextMode">Change mode to {{ nextMode }}</div>
+    <button @click="toggleColorMode"
+            @mouseenter="displayNextMode = true"
+            @mouseleave="displayNextMode = false"
+            class="hover:bg-gray-200 dark:hover:bg-gray-600 px-2 py-1 text-gray-500">{{ nextModeIcon }}
+    </button>
+  </div>
 </template>
 
 <style scoped>
