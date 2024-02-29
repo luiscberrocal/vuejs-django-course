0023cb0caed2d60408e0de2b96e8aa45228ff4cb
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-29 13:28:30 -0500

    Site maps

---
+++ portfolio/server/routes/sitemap.xml.js
@@ -0,0 +1,20 @@
+import { serverQueryContent } from '#content/server'
+import { SitemapStream, streamToPromise } from 'sitemap'
+
+export default defineEventHandler(async (event) => {
+  // Fetch all documents
+  const docs = await serverQueryContent(event).find()
+  const sitemap = new SitemapStream({
+    hostname: 'http://127.0.0.1:3000'
+  })
+
+  for (const doc of docs) {
+    sitemap.write({
+      url: doc._path,
+      changefreq: 'monthly'
+    })
+  }
+  sitemap.end()
+
+  return streamToPromise(sitemap)
+})
