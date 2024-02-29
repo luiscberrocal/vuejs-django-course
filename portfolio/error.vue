d8346ba4510bf8b16a1ed3d647deb1e66ba7d6ac
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-02-29 09:33:19 -0500

    Wrapping error in layout.

--- portfolio/error.vue
+++ portfolio/error.vue
@@ -7,12 +7,14 @@ const handleClearError = () => {
 </script>
 
 <template>
-  <div class="mt-7 max-w-sm mx-auto text-center card">
-    <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
-    <p class="mt-7 text-6xl">Ooops</p>
-    <p class="mt-7">{{ error.message }}</p>
-    <button class="btn" @click="handleClearError">Got home</button>
-  </div>
+  <NuxtLayout>
+    <div class="mt-7 max-w-sm mx-auto text-center card">
+      <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
+      <p class="mt-7 text-6xl">Ooops</p>
+      <p class="mt-7">{{ error.message }}</p>
+      <button class="btn" @click="handleClearError">Got home</button>
+    </div>
+  </NuxtLayout>
 </template>
 
 <style scoped>
