d332fea869daafc79d1e65b4eaa49bfa68aa8f16
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-03-01 09:15:35 -0500

    Headers and avatar

---
+++ finance-tracker/components/app-header.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <header class="flex justify-between items-center">
+    <NuxtLink to="/" class="text-xl font-bold">Finance Tracker</NuxtLink>
+    <UAvatar
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+    />
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
--- finance-tracker/layouts/default.vue
+++ finance-tracker/layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container mx-auto max-w-4xl">
+    <AppHeader></AppHeader>
     <main class="my-10">
-      <slot />
+      <slot/>
     </main>
   </div>
 </template>
@@ -28,6 +29,7 @@ useHead({
 body {
   font-family: 'Inter';
 }
+
 /* Set the background color for the entire site */
 body {
   @apply dark:bg-gray-900 bg-white
