cad1f16506d6fb226108912f204de533876a8a16
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-03-01 17:25:13 -0500

    Calculating totals.

--- finance-tracker/pages/index.vue
+++ finance-tracker/pages/index.vue
@@ -1,10 +1,21 @@
-<script lang="ts" setup>
+<script setup>
 
 import {viewOptions} from "~/constants";
 
 const selectedView = ref(viewOptions[1]);
-const supabase = useSupabaseClient();
 const isLoading = ref(false);
+const transactions = ref([])
+
+const income = computed(() => transactions.value.filter(transaction => transaction.type === 'Income'));
+const expense = computed(() => transactions.value.filter(transaction => transaction.type === 'Expense'));
+
+const incomeCount = computed(() => income.value.length);
+const expenseCount = computed(() => expense.value.length);
+
+const incomeTotal = computed(() => income.value.reduce((sum, transaction) => sum + transaction.amount, 0));
+const expenseTotal = computed(() => expense.value.reduce((sum, transaction) => sum + transaction.amount, 0));
+
+const supabase = useSupabaseClient();
 
 const fetchTransactions = async () => {
   isLoading.value = true;
@@ -25,9 +36,6 @@ const fetchTransactions = async () => {
   }
 }
 
-//console.log('data', data, 'error', error);
-
-const transactions = ref([])
 const refreshTransactions = async () => transactions.value = await fetchTransactions();
 
 await refreshTransactions()
@@ -54,8 +62,8 @@ console.log(transactionsGroupedByDate.value)
     </div>
   </section>
   <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-    <Trend title="Income" color="green" :amount="3000" last-amount="5000" :loading="isLoading"/>
-    <Trend title="Expense" color="red" :amount="3000" last-amount="3000" :loading="isLoading"/>
+    <Trend title="Income" color="green" :amount="incomeTotal" last-amount="5000" :loading="isLoading"/>
+    <Trend title="Expense" color="red" :amount="expenseTotal" last-amount="3000" :loading="isLoading"/>
     <Trend title="Investments" color="green" :amount="3000" last-amount="2000" :loading="isLoading"/>
     <Trend title="Savings" color="red" :amount="3000" last-amount="4000" :loading="isLoading"/>
   </section>
