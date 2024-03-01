c733e67cad35ddf86e6e42eca7a20ca989529a92
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-03-01 11:21:02 -0500

    Finish transaction component.

--- finance-tracker/components/transaction.vue
+++ finance-tracker/components/transaction.vue
@@ -21,7 +21,7 @@ const items = [
 </script>
 
 <template>
-  <div class="grid grid-cols-2 py-4 border-b">
+  <div class="grid grid-cols-2 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-900 dark:text-gray-100">
     <div class="flex items-center justify-between">
       <div class="flex items-center space-x-1">
         <UIcon name="i-heroicons-arrow-up-right" class="text-green-600"></UIcon>
