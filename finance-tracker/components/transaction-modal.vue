b5ce41162b76db7c06753ffa538302494311e82a
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-03-02 07:35:06 -0500

    State for form.

--- finance-tracker/components/transaction-modal.vue
+++ finance-tracker/components/transaction-modal.vue
@@ -12,6 +12,15 @@ const isOpen = computed(
       set: (value) => emit('update:modelValue', value)
     }
 )
+
+const state = ref({
+  type: 'Expense',
+  amount: 0,
+  created_at: new Date().toISOString().split('T')[0],
+  description: undefined,
+  category: undefined
+})
+
 </script>
 
 <template>
@@ -20,22 +29,24 @@ const isOpen = computed(
       <template #header>
         Add Transaction
       </template>
-      <UFormGroup label="Transaction type" name="type" :required="true" class="mb-4">
-        <USelect placeholder="Transaction type" :options="transactionTypes"></USelect>
-      </UFormGroup>
-      <UFormGroup label="Amount" name="amount" :required="true" class="mb-4">
-        <UInput type="number" placeholder="Enter amount"/>
-      </UFormGroup>
-      <UFormGroup label="Transaction date" name="created_at" :required="true" class="mb-4">
-        <UInput type="date" placeholder="Date" icon="i-heroicons-calendar-days-20-solid"/>
-      </UFormGroup>
-      <UFormGroup label="Description" name="description" :required="false" class="mb-4">
-        <UInput type="text" placeholder="Transaction description"/>
-      </UFormGroup>
-      <UFormGroup label="Category" name="category" :required="true" class="mb-4">
-        <USelect placeholder="Categories" :options="categories"></USelect>
-      </UFormGroup>
-      <UButton type="submit" color="black" variant="solid">Save</UButton>
+      <UForm :state="state">
+        <UFormGroup label="Transaction type" name="type" :required="true" class="mb-4">
+          <USelect placeholder="Transaction type" :options="transactionTypes" v-model="state.type"></USelect>
+        </UFormGroup>
+        <UFormGroup label="Amount" name="amount" :required="true" class="mb-4">
+          <UInput type="number" placeholder="Enter amount" v-model.number="state.amount"/>
+        </UFormGroup>
+        <UFormGroup label="Transaction date" name="created_at" :required="true" class="mb-4">
+          <UInput type="date" placeholder="Date" icon="i-heroicons-calendar-days-20-solid" v-model="state.created_at"/>
+        </UFormGroup>
+        <UFormGroup label="Description" name="description" :required="false" class="mb-4">
+          <UInput type="text" placeholder="Transaction description" v-model="state.description"/>
+        </UFormGroup>
+        <UFormGroup label="Category" name="category" :required="true" class="mb-4">
+          <USelect placeholder="Categories" :options="categories" v-model="state.category"></USelect>
+        </UFormGroup>
+        <UButton type="submit" color="black" variant="solid">Save</UButton>
+      </UForm>
     </UCard>
   </UModal>
 
