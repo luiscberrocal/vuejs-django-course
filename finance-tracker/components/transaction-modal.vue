7610724d8e02172b3c5348e04827e06b3dc7d77e
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-03-11 08:13:35 -0500

    Form not working.

--- finance-tracker/components/transaction-modal.vue
+++ finance-tracker/components/transaction-modal.vue
@@ -7,34 +7,34 @@
 
       <UForm :state="state" :schema="schema" ref="form" @submit.prevent="save">
         <UFormGroup :required="true" label="Transaction Type" name="type" class="mb-4">
-          <USelect placeholder="Select the transaction type" :options="types" v-model="state.type" />
+          <USelect placeholder="Select the transaction type" :options="types" v-model="state.type"/>
         </UFormGroup>
 
         <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
-          <UInput type="number" placeholder="Amount" v-model.number="state.amount" />
+          <UInput type="number" placeholder="Amount" v-model.number="state.amount"/>
         </UFormGroup>
 
         <UFormGroup label="Transaction date" :required="true" name="created_at" class="mb-4">
-          <UInput type="date" icon="i-heroicons-calendar-days-20-solid" v-model="state.created_at" />
+          <UInput type="date" icon="i-heroicons-calendar-days-20-solid" v-model="state.created_at"/>
         </UFormGroup>
 
         <UFormGroup label="Description" hint="Optional" name="description" class="mb-4">
-          <UInput placeholder="Description" v-model="state.description" />
+          <UInput placeholder="Description" v-model="state.description"/>
         </UFormGroup>
 
         <UFormGroup :required="true" label="Category" name="category" class="mb-4" v-if="state.type === 'Expense'">
-          <USelect placeholder="Category" :options="categories" v-model="state.category" />
+          <USelect placeholder="Category" :options="categories" v-model="state.category"/>
         </UFormGroup>
 
-        <UButton type="submit" color="black" variant="solid" label="Save" :loading="isLoading" />
+        <UButton type="submit" color="black" variant="solid" label="Save" :loading="isLoading"/>
       </UForm>
     </UCard>
   </UModal>
 </template>
 
 <script setup>
-import { categories, types } from '~/constants'
-import { z } from 'zod'
+import {categories, types} from '~/constants'
+import {z} from 'zod'
 
 const props = defineProps({
   modelValue: Boolean
@@ -62,22 +62,28 @@ const savingSchema = z.object({
 })
 
 const schema = z.intersection(
-  z.discriminatedUnion('type', [incomeSchema, expenseSchema, investmentSchema, savingSchema]),
-  defaultSchema
+    z.discriminatedUnion('type', [incomeSchema, expenseSchema, investmentSchema, savingSchema]),
+    defaultSchema
 )
 
 const form = ref()
 const isLoading = ref(false)
 const supabase = useSupabaseClient()
 const toast = useToast()
-
+const dd = async () => {
+  console.log('>>> STATE', state.value);
+  console.log('>>> FORM', form.value);
+}
 const save = async () => {
+  console.log('>>> STATE', state.value);
+  console.log('>>> FORM', form.value);
   if (form.value.errors.length) return
 
   isLoading.value = true
   try {
-    const { error } = await supabase.from('transactions')
-      .upsert({ ...state.value })
+    console.log('>>> STATE', state.value);
+    const {error} = await supabase.from('transactions')
+        .upsert({...state.value})
 
     if (!error) {
       toast.add({
@@ -103,9 +109,9 @@ const save = async () => {
 }
 
 const initialState = {
-  type: undefined,
+  type: "Income",//undefined,
   amount: 0,
-  created_at: undefined,
+  created_at: new Date().toLocaleDateString(), //undefined,
   description: undefined,
   category: undefined
 }
