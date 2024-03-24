a5cf88cbd13e65523c9136e8431c6fd9a87e38cc
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-03-24 11:02:13 -0500

    Calisthenics

---
+++ finance-tracker/components/calisthenics/exercise-summary.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const props = defineProps(
+    {
+      name: {
+        type: String,
+        required: true
+      },
+      average: {
+        type: Number,
+        required: true
+      },
+      todaysCount: {
+        type: Number,
+        required: true
+      }
+    }
+)
+</script>
+
+<template>
+  <div>
+    <h1>{{ name }} Summary</h1>
+    <p>Average: {{ average }}</p>
+    <p>Today's Count: {{ todaysCount }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- finance-tracker/pages/calisthenics/index.vue
+++ finance-tracker/pages/calisthenics/index.vue
@@ -1,10 +1,33 @@
 <script lang="ts" setup>
-
+const exercisesSummaries = [
+  {
+    name: 'Pushups',
+    average: 100,
+    todaysCount: 50
+  },
+  {
+    name: 'Pullups',
+    average: 50,
+    todaysCount: 25
+  },
+  {
+    name: 'Squats',
+    average: 200,
+    todaysCount: 100
+  }
+]
 </script>
 
 <template>
   <div>
     <h1>Calisthenics</h1>
+    <div v-for="exercise in exercisesSummaries" :key="exercise.name">
+      <CalisthenicsExerciseSummary
+          :name="exercise.name"
+          :average="exercise.average"
+          :todaysCount="exercise.todaysCount"
+      />
+    </div>
   </div>
 </template>
 
